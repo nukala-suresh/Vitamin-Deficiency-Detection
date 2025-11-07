a5ab11aa4b9edd4f0e8e77e1298cff24b5a07f39
nukala-suresh <suresh.nukala99@gmail.com>
2025-11-07 15:19:03 +0530

    Add files via upload

--- __init__.py
+++ __init__.py
@@ -1,169 +1,157 @@
-from .beit import *
-from .byoanet import *
-from .byobnet import *
-from .cait import *
-from .coat import *
-from .convit import *
-from .convmixer import *
-from .convnext import *
-from .crossvit import *
-from .cspnet import *
-from .davit import *
-from .deit import *
-from .densenet import *
-from .dla import *
-from .dpn import *
-from .edgenext import *
-from .efficientformer import *
-from .efficientformer_v2 import *
-from .efficientnet import *
-from .efficientvit_mit import *
-from .efficientvit_msra import *
-from .eva import *
-from .fasternet import *
-from .fastvit import *
-from .focalnet import *
-from .gcvit import *
-from .ghostnet import *
-from .hardcorenas import *
-from .hgnet import *
-from .hiera import *
-from .hieradet_sam2 import *
-from .hrnet import *
-from .inception_next import *
-from .inception_resnet_v2 import *
-from .inception_v3 import *
-from .inception_v4 import *
-from .levit import *
-from .maxxvit import *
-from .mambaout import *
-from .metaformer import *
-from .mlp_mixer import *
-from .mobilenetv3 import *
-from .mobilenetv5 import *
-from .mobilevit import *
-from .mvitv2 import *
-from .naflexvit import *
-from .nasnet import *
-from .nest import *
-from .nextvit import *
-from .nfnet import *
-from .pit import *
-from .pnasnet import *
-from .pvt_v2 import *
-from .rdnet import *
-from .regnet import *
-from .repghost import *
-from .repvit import *
-from .res2net import *
-from .resnest import *
-from .resnet import *
-from .resnetv2 import *
-from .rexnet import *
-from .selecsls import *
-from .senet import *
-from .sequencer import *
-from .shvit import *
-from .sknet import *
-from .starnet import *
-from .swiftformer import *
-from .swin_transformer import *
-from .swin_transformer_v2 import *
-from .swin_transformer_v2_cr import *
-from .tiny_vit import *
-from .tnt import *
-from .tresnet import *
-from .twins import *
-from .vgg import *
-from .visformer import *
-from .vision_transformer import *
-from .vision_transformer_hybrid import *
-from .vision_transformer_relpos import *
-from .vision_transformer_sam import *
-from .vitamin import *
-from .volo import *
-from .vovnet import *
-from .xception import *
-from .xception_aligned import *
-from .xcit import *
-
-from ._builder import (
-    build_model_with_cfg as build_model_with_cfg,
-    load_pretrained as load_pretrained,
-    load_custom_pretrained as load_custom_pretrained,
-    resolve_pretrained_cfg as resolve_pretrained_cfg,
-    set_pretrained_download_progress as set_pretrained_download_progress,
-    set_pretrained_check_hash as set_pretrained_check_hash,
+from ._fx import (
+    create_feature_extractor,
+    get_graph_node_names,
+    register_notrace_function,
+    register_notrace_module,
+    is_notrace_module,
+    is_notrace_function,
+    get_notrace_modules,
+    get_notrace_functions,
 )
-from ._factory import (
-    create_model as create_model,
-    parse_model_name as parse_model_name,
-    safe_model_name as safe_model_name,
+from .activations import *
+from .adaptive_avgmax_pool import (
+    adaptive_avgmax_pool2d,
+    select_adaptive_pool2d,
+    AdaptiveAvgMaxPool2d,
+    SelectAdaptivePool2d,
 )
-from ._features import (
-    FeatureInfo as FeatureInfo,
-    FeatureHooks as FeatureHooks,
-    FeatureHookNet as FeatureHookNet,
-    FeatureListNet as FeatureListNet,
-    FeatureDictNet as FeatureDictNet,
+from .attention import Attention, AttentionRope, maybe_add_mask
+from .attention2d import MultiQueryAttention2d, Attention2d, MultiQueryAttentionV2
+from .attention_pool import AttentionPoolLatent
+from .attention_pool2d import AttentionPool2d, RotAttentionPool2d, RotaryEmbedding
+from .blur_pool import BlurPool2d, create_aa
+from .classifier import create_classifier, ClassifierHead, NormMlpClassifierHead, ClNormMlpClassifierHead
+from .cond_conv2d import CondConv2d, get_condconv_initializer
+from .config import (
+    is_exportable,
+    is_scriptable,
+    is_no_jit,
+    use_fused_attn,
+    set_exportable,
+    set_scriptable,
+    set_no_jit,
+    set_layer_config,
+    set_fused_attn,
+    set_reentrant_ckpt,
+    use_reentrant_ckpt,
 )
-from ._features_fx import (
-    FeatureGraphNet as FeatureGraphNet,
-    GraphExtractNet as GraphExtractNet,
-    create_feature_extractor as create_feature_extractor,
-    get_graph_node_names as get_graph_node_names,
-    register_notrace_module as register_notrace_module,
-    is_notrace_module as is_notrace_module,
-    get_notrace_modules as get_notrace_modules,
-    register_notrace_function as register_notrace_function,
-    is_notrace_function as is_notrace_function,
-    get_notrace_functions as get_notrace_functions,
+from .conv2d_same import Conv2dSame, conv2d_same
+from .conv_bn_act import ConvNormAct, ConvNormActAa, ConvBnAct
+from .create_act import create_act_layer, get_act_layer, get_act_fn
+from .create_attn import get_attn, create_attn
+from .create_conv2d import create_conv2d
+from .create_norm import get_norm_layer, create_norm_layer
+from .create_norm_act import get_norm_act_layer, create_norm_act_layer, get_norm_act_layer
+from .drop import DropBlock2d, DropPath, drop_block_2d, drop_path, calculate_drop_path_rates
+from .eca import EcaModule, CecaModule, EfficientChannelAttn, CircularEfficientChannelAttn
+from .evo_norm import (
+    EvoNorm2dB0,
+    EvoNorm2dB1,
+    EvoNorm2dB2,
+    EvoNorm2dS0,
+    EvoNorm2dS0a,
+    EvoNorm2dS1,
+    EvoNorm2dS1a,
+    EvoNorm2dS2,
+    EvoNorm2dS2a,
 )
-from ._helpers import (
-    clean_state_dict as clean_state_dict,
-    load_state_dict as load_state_dict,
-    load_checkpoint as load_checkpoint,
-    remap_state_dict as remap_state_dict,
-    resume_checkpoint as resume_checkpoint,
+from .fast_norm import is_fast_norm, set_fast_norm, fast_group_norm, fast_layer_norm
+from .filter_response_norm import FilterResponseNormTlu2d, FilterResponseNormAct2d
+from .format import Format, get_channel_dim, get_spatial_dim, nchw_to, nhwc_to
+from .gather_excite import GatherExcite
+from .global_context import GlobalContext
+from .grid import ndgrid, meshgrid
+from .helpers import to_ntuple, to_2tuple, to_3tuple, to_4tuple, make_divisible, extend_tuple
+from .hybrid_embed import HybridEmbed, HybridEmbedWithSize
+from .inplace_abn import InplaceAbn
+from .layer_scale import LayerScale, LayerScale2d
+from .linear import Linear
+from .mixed_conv2d import MixedConv2d
+from .mlp import Mlp, GluMlp, GatedMlp, SwiGLU, SwiGLUPacked, ConvMlp, GlobalResponseNormMlp
+from .non_local_attn import NonLocalAttn, BatNonLocalAttn
+from .norm import (
+    GroupNorm,
+    GroupNorm1,
+    LayerNorm,
+    LayerNorm2d,
+    LayerNormFp32,
+    LayerNorm2dFp32,
+    RmsNorm,
+    RmsNorm2d,
+    RmsNormFp32,
+    RmsNorm2dFp32,
+    SimpleNorm,
+    SimpleNorm2d,
+    SimpleNormFp32,
+    SimpleNorm2dFp32,
 )
-from ._hub import (
-    load_model_config_from_hf as load_model_config_from_hf,
-    load_state_dict_from_hf as load_state_dict_from_hf,
-    push_to_hf_hub as push_to_hf_hub,
-    save_for_hf as save_for_hf,
+from .norm_act import (
+    BatchNormAct2d,
+    GroupNormAct,
+    GroupNorm1Act,
+    LayerNormAct,
+    LayerNormAct2d,
+    LayerNormActFp32,
+    LayerNormAct2dFp32,
+    RmsNormAct,
+    RmsNormAct2d,
+    RmsNormActFp32,
+    RmsNormAct2dFp32,
+    SyncBatchNormAct,
+    convert_sync_batchnorm,
+    FrozenBatchNormAct2d,
+    freeze_batch_norm_2d,
+    unfreeze_batch_norm_2d,
 )
-from ._manipulate import (
-    model_parameters as model_parameters,
-    named_apply as named_apply,
-    named_modules as named_modules,
-    named_modules_with_params as named_modules_with_params,
-    group_modules as group_modules,
-    group_parameters as group_parameters,
-    checkpoint_seq as checkpoint_seq,
-    checkpoint as checkpoint,
-    adapt_input_conv as adapt_input_conv,
+from .padding import get_padding, get_same_padding, pad_same
+from .patch_dropout import PatchDropout, PatchDropoutWithIndices, patch_dropout_forward
+from .patch_embed import PatchEmbed, PatchEmbedWithSize, PatchEmbedInterpolator, resample_patch_embed
+from .pool1d import global_pool_nlc
+from .pool2d_same import AvgPool2dSame, create_pool2d
+from .pos_embed import resample_abs_pos_embed, resample_abs_pos_embed_nhwc
+from .pos_embed_rel import (
+    RelPosMlp,
+    RelPosBias,
+    RelPosBiasTf,
+    gen_relative_position_index,
+    gen_relative_log_coords,
+    resize_rel_pos_bias_table,
+    resize_rel_pos_bias_table_simple,
+    resize_rel_pos_bias_table_levit,
 )
-from ._pretrained import (
-    PretrainedCfg as PretrainedCfg,
-    DefaultCfg as DefaultCfg,
-    filter_pretrained_cfg as filter_pretrained_cfg,
+from .pos_embed_sincos import (
+    pixel_freq_bands,
+    freq_bands,
+    build_sincos2d_pos_embed,
+    build_fourier_pos_embed,
+    build_rotary_pos_embed,
+    apply_rot_embed,
+    apply_rot_embed_cat,
+    apply_rot_embed_list,
+    apply_keep_indices_nlc,
+    FourierEmbed,
+    RotaryEmbedding,
+    RotaryEmbeddingCat,
+    RotaryEmbeddingMixed,
+    RotaryEmbeddingDinoV3,
+    get_mixed_freqs,
+    create_rope_embed,
 )
-from ._prune import adapt_model_from_string as adapt_model_from_string
-from ._registry import (
-    split_model_name_tag as split_model_name_tag,
-    get_arch_name as get_arch_name,
-    generate_default_cfgs as generate_default_cfgs,
-    register_model as register_model,
-    register_model_deprecations as register_model_deprecations,
-    model_entrypoint as model_entrypoint,
-    list_models as list_models,
-    list_pretrained as list_pretrained,
-    get_deprecated_models as get_deprecated_models,
-    is_model as is_model,
-    list_modules as list_modules,
-    is_model_in_modules as is_model_in_modules,
-    is_model_pretrained as is_model_pretrained,
-    get_pretrained_cfg as get_pretrained_cfg,
-    get_pretrained_cfg_value as get_pretrained_cfg_value,
-    get_arch_pretrained_cfgs as get_arch_pretrained_cfgs,
+from .squeeze_excite import SEModule, SqueezeExcite, EffectiveSEModule, EffectiveSqueezeExcite
+from .selective_kernel import SelectiveKernel
+from .separable_conv import SeparableConv2d, SeparableConvNormAct
+from .space_to_depth import SpaceToDepth, DepthToSpace
+from .split_attn import SplitAttn
+from .split_batchnorm import SplitBatchNorm2d, convert_splitbn_model
+from .std_conv import StdConv2d, StdConv2dSame, ScaledStdConv2d, ScaledStdConv2dSame
+from .test_time_pool import TestTimePoolHead, apply_test_time_pool
+from .trace_utils import _assert, _float_to_int
+from .typing import LayerType, PadType, disable_compiler
+from .weight_init import (
+    trunc_normal_,
+    trunc_normal_tf_,
+    variance_scaling_,
+    lecun_normal_,
+    init_weight_jax,
+    init_weight_vit,
 )
--- config.py
+++ config.py
@@ -1,55 +1,165 @@
-from datetime import timedelta
+""" Model / Layer Config singleton state
+"""
+import os
+import warnings
+from typing import Any, Optional
 
-#: The default name of the "remember me" cookie (``remember_token``)
-COOKIE_NAME = "remember_token"
+import torch
 
-#: The default time before the "remember me" cookie expires (365 days).
-COOKIE_DURATION = timedelta(days=365)
+__all__ = [
+    'is_exportable', 'is_scriptable', 'is_no_jit', 'use_fused_attn',
+    'set_exportable', 'set_scriptable', 'set_no_jit', 'set_layer_config', 'set_fused_attn',
+    'set_reentrant_ckpt', 'use_reentrant_ckpt'
+]
 
-#: Whether the "remember me" cookie requires Secure; defaults to ``False``
-COOKIE_SECURE = False
+# Set to True if prefer to have layers with no jit optimization (includes activations)
+_NO_JIT = False
 
-#: Whether the "remember me" cookie uses HttpOnly or not; defaults to ``True``
-COOKIE_HTTPONLY = True
+# Set to True if prefer to have activation layers with no jit optimization
+# NOTE not currently used as no difference between no_jit and no_activation jit as only layers obeying
+# the jit flags so far are activations. This will change as more layers are updated and/or added.
+_NO_ACTIVATION_JIT = False
 
-#: Whether the "remember me" cookie requires same origin; defaults to ``None``
-COOKIE_SAMESITE = None
+# Set to True if exporting a model with Same padding via ONNX
+_EXPORTABLE = False
 
-#: The default flash message to display when users need to log in.
-LOGIN_MESSAGE = "Please log in to access this page."
+# Set to True if wanting to use torch.jit.script on a model
+_SCRIPTABLE = False
 
-#: The default flash message category to display when users need to log in.
-LOGIN_MESSAGE_CATEGORY = "message"
 
-#: The default flash message to display when users need to reauthenticate.
-REFRESH_MESSAGE = "Please reauthenticate to access this page."
+# use torch.scaled_dot_product_attention where possible
+_HAS_FUSED_ATTN = hasattr(torch.nn.functional, 'scaled_dot_product_attention')
+if 'TIMM_FUSED_ATTN' in os.environ:
+    _USE_FUSED_ATTN = int(os.environ['TIMM_FUSED_ATTN'])
+else:
+    _USE_FUSED_ATTN = 1  # 0 == off, 1 == on (for tested use), 2 == on (for experimental use)
 
-#: The default flash message category to display when users need to
-#: reauthenticate.
-REFRESH_MESSAGE_CATEGORY = "message"
 
-#: The default attribute to retreive the str id of the user
-ID_ATTRIBUTE = "get_id"
+if 'TIMM_REENTRANT_CKPT' in os.environ:
+    _USE_REENTRANT_CKPT = bool(os.environ['TIMM_REENTRANT_CKPT'])
+else:
+    _USE_REENTRANT_CKPT = False  # defaults to disabled (off)
 
-#: Default name of the auth header (``Authorization``)
-AUTH_HEADER_NAME = "Authorization"
 
-#: A set of session keys that are populated by Flask-Login. Use this set to
-#: purge keys safely and accurately.
-SESSION_KEYS = {
-    "_user_id",
-    "_remember",
-    "_remember_seconds",
-    "_id",
-    "_fresh",
-    "next",
-}
+def is_no_jit():
+    return _NO_JIT
 
-#: A set of HTTP methods which are exempt from `login_required` and
-#: `fresh_login_required`. By default, this is just ``OPTIONS``.
-EXEMPT_METHODS = {"OPTIONS"}
 
-#: If true, the page the user is attempting to access is stored in the session
-#: rather than a url parameter when redirecting to the login view; defaults to
-#: ``False``.
-USE_SESSION_FOR_NEXT = False
+class set_no_jit:
+    def __init__(self, mode: bool) -> None:
+        global _NO_JIT
+        self.prev = _NO_JIT
+        _NO_JIT = mode
+
+    def __enter__(self) -> None:
+        pass
+
+    def __exit__(self, *args: Any) -> bool:
+        global _NO_JIT
+        _NO_JIT = self.prev
+        return False
+
+
+def is_exportable():
+    return _EXPORTABLE
+
+
+class set_exportable:
+    def __init__(self, mode: bool) -> None:
+        global _EXPORTABLE
+        self.prev = _EXPORTABLE
+        _EXPORTABLE = mode
+
+    def __enter__(self) -> None:
+        pass
+
+    def __exit__(self, *args: Any) -> bool:
+        global _EXPORTABLE
+        _EXPORTABLE = self.prev
+        return False
+
+
+def is_scriptable():
+    return _SCRIPTABLE
+
+
+class set_scriptable:
+    def __init__(self, mode: bool) -> None:
+        global _SCRIPTABLE
+        self.prev = _SCRIPTABLE
+        _SCRIPTABLE = mode
+
+    def __enter__(self) -> None:
+        pass
+
+    def __exit__(self, *args: Any) -> bool:
+        global _SCRIPTABLE
+        _SCRIPTABLE = self.prev
+        return False
+
+
+class set_layer_config:
+    """ Layer config context manager that allows setting all layer config flags at once.
+    If a flag arg is None, it will not change the current value.
+    """
+    def __init__(
+            self,
+            scriptable: Optional[bool] = None,
+            exportable: Optional[bool] = None,
+            no_jit: Optional[bool] = None,
+            no_activation_jit: Optional[bool] = None):
+        global _SCRIPTABLE
+        global _EXPORTABLE
+        global _NO_JIT
+        global _NO_ACTIVATION_JIT
+        self.prev = _SCRIPTABLE, _EXPORTABLE, _NO_JIT, _NO_ACTIVATION_JIT
+        if scriptable is not None:
+            _SCRIPTABLE = scriptable
+        if exportable is not None:
+            _EXPORTABLE = exportable
+        if no_jit is not None:
+            _NO_JIT = no_jit
+        if no_activation_jit is not None:
+            _NO_ACTIVATION_JIT = no_activation_jit
+
+    def __enter__(self) -> None:
+        pass
+
+    def __exit__(self, *args: Any) -> bool:
+        global _SCRIPTABLE
+        global _EXPORTABLE
+        global _NO_JIT
+        global _NO_ACTIVATION_JIT
+        _SCRIPTABLE, _EXPORTABLE, _NO_JIT, _NO_ACTIVATION_JIT = self.prev
+        return False
+
+
+def use_fused_attn(experimental: bool = False) -> bool:
+    # NOTE: ONNX export cannot handle F.scaled_dot_product_attention as of pytorch 2.0
+    if not _HAS_FUSED_ATTN or _EXPORTABLE:
+        return False
+    if experimental:
+        return _USE_FUSED_ATTN > 1
+    return _USE_FUSED_ATTN > 0
+
+
+def set_fused_attn(enable: bool = True, experimental: bool = False):
+    global _USE_FUSED_ATTN
+    if not _HAS_FUSED_ATTN:
+        warnings.warn('This version of pytorch does not have F.scaled_dot_product_attention, fused_attn flag ignored.')
+        return
+    if experimental and enable:
+        _USE_FUSED_ATTN = 2
+    elif enable:
+        _USE_FUSED_ATTN = 1
+    else:
+        _USE_FUSED_ATTN = 0
+
+
+def use_reentrant_ckpt() -> bool:
+    return _USE_REENTRANT_CKPT
+
+
+def set_reentrant_ckpt(enable: bool = True):
+    global _USE_REENTRANT_CKPT
+    _USE_REENTRANT_CKPT = enable
